2ad3ed25531063fc108006e254c40b309e8d3af7
Xang Hang <hangx@students.westerntc.edu>
2024-04-03 14:44:08 -0500

    class updates

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,23 @@
-console.log("This is main.js...");
+// console.log("This is main.js...");
+
+// window.addEventListener('load', function(){
+// 	// JavaScript code goes here
+
+// 	var btnNav = document.getElementById("mobile-nav-button");
+// 	var mainNav = document.getElementById("main-nav");
+
+// 	window.addEventListener('click', function(evt){
+		
+// 		if(evt.target == btnNav){
+
+// 			if(mainNav.classList.contains("show")){
+// 				mainNav.classList.remove("show");
+// 			}else{
+// 				mainNav.classList.add("show");
+// 			}
+// 		}else{
+// 			mainNav.classList.remove("show");
+// 		}
+// 	});
+
+// });
